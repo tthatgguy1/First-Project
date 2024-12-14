d7cf7865fa7ebf777afdb0d03f67a575145791d8
tthatgguy1 <68300078+tthatgguy1@users.noreply.github.com>
2024-12-14 07:10:53 -0600

    Set-Vars.bat

--- Set-Vars.bat
+++ Set-Vars.bat
@@ -56,6 +56,8 @@
         set COLOR_BG_GREEN=[42m>nul
         set COLOR_BG_PURPLE=[45m>nul
         set COLOR_BG_RED=[41m>nul
+        set COLOR_BG_BLK_RED=[1;5;41m>nul
+        set COLOR_BG_BLK2_RED=[1;6;41m>nul
         set COLOR_BG_WHITE=[47m>nul
         set COLOR_BG_YELLOW=[43m>nul
         set COLOR_RESET=[0m>nul
