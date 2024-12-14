127bbf98c1e725b6aa3177c23a5336bf8a4b5f8f
tthatgguy1 <68300078+tthatgguy1@users.noreply.github.com>
2024-12-14 08:15:34 -0600

    Next_Gem_Crypto_Gaming.bat

--- Next_Gem_Crypto_Gaming.bat
+++ Next_Gem_Crypto_Gaming.bat
@@ -8,7 +8,7 @@
     echo                                                           %COLOR_GOLDMINER%M@%COLOR_RESET%     %COLOR_GOLDMINER%@O%COLOR_RESET%                                                           
     PING localhost -n 1 >nul
     echo                                                          %COLOR_GOLDMINER%b@%COLOR_RESET%      %COLOR_GOLDMINER%.@s%COLOR_RESET%             %COLOR_DARK_GREEN%;UEZFl7%COLOR_RESET%                                      
-    PING localhost -n 2 >nul
+    PING localhost -n 1 >nul
     echo                                                         %COLOR_GOLDMINER%h@%COLOR_RESET%        %COLOR_GOLDMINER%,@v%COLOR_RESET%             %COLOR_DARK_GREEN%;@@G@@,%COLOR_RESET%                                     
     PING localhost -n 1 >nul
     echo                                                        %COLOR_GOLDMINER%P@%COLOR_RESET%          %COLOR_GOLDMINER%,@5%COLOR_RESET%               %COLOR_DARK_GREEN%,%COLOR_RESET%                                        
@@ -24,7 +24,7 @@
     echo                                                   %COLOR_GOLDMINER%;7%COLOR_RESET%   %COLOR_DARK_GREEN%.;7YYc77;Yvv7T5FSCSCc:%COLOR_RESET%      %COLOR_DARK_GREEN%:7Sc7,%COLOR_RESET%                                    
     PING localhost -n 1 >nul
     echo                                                  %COLOR_GOLDMINER%;%COLOR_RESET%    %COLOR_DARK_GREEN%;;;:.%COLOR_RESET%       %COLOR_DARK_GREEN%.;;7752XC22s;,.;7TXS;,%COLOR_RESET%                                     
-    PING localhost -n 2 >nul
+    PING localhost -n 1 >nul
     echo                                                 %COLOR_GOLDMINER%77%COLOR_RESET%   %COLOR_DARK_GREEN%,,;YU0M@@@@@U;%COLOR_RESET%    %COLOR_DARK_GREEN%,7vFGZFOS5sS21;.%COLOR_RESET%                                      
     PING localhost -n 1 >nul
     echo                                                   %COLOR_DARK_GREEN%:Yh@@@@@@@@@@@@@@@d7%COLOR_RESET%    %COLOR_DARK_GREEN%:;lZF5llc7:%COLOR_RESET%                                        
@@ -50,7 +50,7 @@
     echo                                       %COLOR_GOLDMINER%17               %COLOR_GOLDMINER%s5%COLOR_RESET%          %COLOR_GOLDMINER%Y1%COLOR_RESET%               %COLOR_GOLDMINER%7Y%COLOR_RESET%                                      
     PING localhost -n 1 >nul
     echo                                      %COLOR_GOLDMINER%l7                 %COLOR_GOLDMINER%E2%COLOR_RESET%        %COLOR_GOLDMINER%71%COLOR_RESET%                 %COLOR_GOLDMINER%77%COLOR_RESET%                                     
-    PING localhost -n 2 >nul
+    PING localhost -n 1 >nul
     echo                                     %COLOR_GOLDMINER%Y7                   %COLOR_GOLDMINER%Y;%COLOR_RESET%      %COLOR_GOLDMINER%;1.%COLOR_RESET%                  %COLOR_GOLDMINER%;;%COLOR_RESET%                                    
     PING localhost -n 1 >nul
     echo                                    %COLOR_GOLDMINER%77 .. ...   . .   .   ,Y;%COLOR_RESET%    %COLOR_GOLDMINER%;l,%COLOR_RESET%   %COLOR_GOLDMINER%. ...  ....%COLOR_RESET%      %COLOR_GOLDMINER%5l%COLOR_RESET%                                    
@@ -74,7 +74,7 @@
     echo                    %COLOR_SLB_PURPLE%,9@@9:  @@  5@#; PMZhs%COLOR_RESET%    %COLOR_FBLK_BRIGHTPURPLE%YM@T   @9  ;MDZP;U@. b@U ;@M9:%COLOR_RESET%     %COLOR_SLB_PURPLE%:E@Gh@Z   XMZ95 #@. M@, G@#7%COLOR_RESET%                   
     PING localhost -n 1 >nul
     echo.                                                                                                                             
-    PING localhost -n 2 >nul
+    PING localhost -n 1 >nul
     echo.                                                                                                                             
     PING localhost -n 1 >nul
     echo.                                                                                                                             
