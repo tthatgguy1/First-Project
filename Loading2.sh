dc64fff92b71ce0476610273b4857bd21dd56af3
tthatgguy1 <68300078+tthatgguy1@users.noreply.github.com>
2024-12-15 20:51:03 -0600

    Loading2.sh

--- Loading2.sh
+++ Loading2.sh
@@ -1,8 +1,364 @@
-echo " "
-echo " "
-echo " "
-echo " "
-echo " "
+GNU nano 8.2                               /data/data/com.termux/files/usr/etc/bash.bashrc                            Modified
+command_not_found_handle() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+PS1='\033[1;95mBoss\033[0m\033[1;36mDaddy\033[0m\033[1;94m>\033[0m\033[1;94m>\033[0m'
+clear
+echo ""
+sleep 0.5
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/███08%................../
+sleep 0.6
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/██████20%.............../
+sleep 0.7
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/████████29%............./
+sleep 0.9
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/█████████33%............/
+sleep 1.0
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/███████████41%........../
+sleep 1.2
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/█████████████54%......../
+sleep 1.4
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/██████████████58%......./
+sleep 1.6
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/████████████████66%...../
+sleep 1.8
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/██████████████████75%.../
+sleep 2.0
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/███████████████████79%../
+sleep 1.0
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/█████████████████████87%/
+sleep 0.8
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/██████████████████████../ 91%
+sleep 0.7
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                             ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                             │  │ │├─┤ │││││││ ┬
+                             ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                     "/███████████████████████./ 93%
+sleep 1.0
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
 echo '
                   _                    _
                  | |                  | |
@@ -10,25 +366,23 @@ echo '
                  |  _ \ / _ \/ _` / __| __|
                  | |_) |  __/ (_| \__ \ |_
                  |_.__/ \___|\__,_|___/\__|'|lolcat
-'
-echo " "
-echo " "
-echo "               "[██████████████████████ 100%]
-echo " "
-echo " "
-echo " "
-echo " "
-echo " "
-echo " "
+
+echo ""
+echo ""
+echo "                     "[████████████████████████]
+echo "                               "100%
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
 sleep 1.0
 mpv /sdcard/scifi.mp3
 clear
 cowsay -f eyes Daddy | lolcat
 toilet -f big '  Boss' -F gay | lolcat
-echo " "
-echo " "
-echo                               '\033[4;31m\033\ date \[0m'
-echo " "
-echo                      '\033[1;95m⚡\033[0m''\[033[1;93m127.0.0.1\033[0m' '\>
-echo " "
-echo " "
+echo ""
+echo ""
+echo                                            '\033[4;31m\033\ date \[0m'
+echo ""
+echo"                      " '\033[1;95m⚡\033[0m''\[033[1;93m127.0.0.1\033[0m' '\033[1;95m⚡\033[0m
