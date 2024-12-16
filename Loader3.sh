304136e033ac5981ddbf4dd571b4710f36b70dd7
tthatgguy1 <68300078+tthatgguy1@users.noreply.github.com>
2024-12-15 20:34:52 -0600

    Create Loader3.sh
    3rd

---
+++ Loader3.sh
@@ -0,0 +1,39 @@
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+                  _                    _
+                 | |                  | |
+                 | |__   ___  __ _ ___| |_
+                 |  _ \ / _ \/ _` / __| __|
+                 | |_) |  __/ (_| \__ \ |_
+                 |_.__/ \___|\__,_|___/\__|'|lolcat
+
+echo ""
+echo ""
+echo "               "[██████████████████████ 100%]
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1.0
+mpv /sdcard/scifi.mp3
+clear
+cowsay -f eyes  | lolcat
+toilet -f big '  ' -F gay | lolcat
+echo -e "\e[96m"
+echo " "
+date
+echo " "
+echo "                      "⚡ 127.0.0.1 ⚡
+echo " "
+echo -e "\e[32m"
