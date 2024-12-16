ff1a3c632085fe0c2f8120b5e1667275bdb38b33
tthatgguy1 <68300078+tthatgguy1@users.noreply.github.com>
2024-12-15 20:21:13 -0600

    Loading2.sh

---
+++ Loading2.sh
@@ -0,0 +1,34 @@
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                  _                    _
+                 | |                  | |
+                 | |__   ___  __ _ ___| |_
+                 |  _ \ / _ \/ _` / __| __|
+                 | |_) |  __/ (_| \__ \ |_
+                 |_.__/ \___|\__,_|___/\__|'|lolcat
+'
+echo " "
+echo " "
+echo "               "[██████████████████████ 100%]
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+sleep 1.0
+mpv /sdcard/scifi.mp3
+clear
+cowsay -f eyes Daddy | lolcat
+toilet -f big '  Boss' -F gay | lolcat
+echo " "
+echo " "
+echo                               '\033[4;31m\033\ date \[0m'
+echo " "
+echo                      '\033[1;95m⚡\033[0m''\[033[1;93m127.0.0.1\033[0m' '\>
+echo " "
+echo " "
